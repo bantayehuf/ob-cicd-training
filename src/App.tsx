01bbef0bc09cdc3d5386ef848a5a459f177822ed
bantayehuf <bantayehuf@gmail.com>
2024-03-04 23:32:13 +0300

    Added sub title

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className='main-container'>
       <h1>Welcome to OB Training!</h1>
+      <h3>By: Bantayehu Fikadu</h3>
     </div>
   )
 }
--- src/assets/App.test.tsx
+++ src/assets/App.test.tsx
@@ -1,3 +1,3 @@
 test('demo', () => {
-    expect(true).toBe(false)
+    expect(true).toBe(true)
 })
